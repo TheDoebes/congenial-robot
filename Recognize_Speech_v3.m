f006d585af13afc67c8dd8616a36f391d2328800
TheDoebes <TheDoebes@users.noreply.github.com>
2019-01-21 16:16:12 -0500

    Update Recognize_Speech_v3.m

--- Recognize_Speech_v3.m
+++ Recognize_Speech_v3.m
@@ -21,52 +21,56 @@ word_in_original = word_in;
 i = 1;
 while (length(word_in)>min(len))
     if(length(word_in) < min(len))
-    break
+        break
     elseif(length(word_in) > max(len))
-[one_corr, onelags] = xcorr(word_in(1:len(2)), one_ref);
-[two_corr, twolags] = xcorr(word_in(1:len(3)), two_ref);
-[three_corr, threelags] = xcorr(word_in(1:len(4)), three_ref);
-[four_corr, fourlags] = xcorr(word_in(1:len(5)), four_ref);
-[five_corr, fivelags] = xcorr(word_in(1:len(6)), five_ref);
-[six_corr, sixlags] = xcorr(word_in(1:len(7)), six_ref);
-[seven_corr, sevenlags] = xcorr(word_in(1:len(8)), seven_ref);
-[eight_corr, eightlags] = xcorr(word_in(1:len(9)), eight_ref);
-[nine_corr, ninelags] = xcorr(word_in(1:len(10)), nine_ref);
-[zero_corr, zerolags] = xcorr(word_in(1:len(1)), zero_ref);
+        [one_corr, onelags] = xcorr(word_in(1:len(2)), one_ref);
+        [two_corr, twolags] = xcorr(word_in(1:len(3)), two_ref);
+        [three_corr, threelags] = xcorr(word_in(1:len(4)), three_ref);
+        [four_corr, fourlags] = xcorr(word_in(1:len(5)), four_ref);
+        [five_corr, fivelags] = xcorr(word_in(1:len(6)), five_ref);
+        [six_corr, sixlags] = xcorr(word_in(1:len(7)), six_ref);
+        [seven_corr, sevenlags] = xcorr(word_in(1:len(8)), seven_ref);
+        [eight_corr, eightlags] = xcorr(word_in(1:len(9)), eight_ref);
+        [nine_corr, ninelags] = xcorr(word_in(1:len(10)), nine_ref);
+        [zero_corr, zerolags] = xcorr(word_in(1:len(1)), zero_ref);
     else %When the remaining word in is not longer than all references but not shorter than them all either
-[one_corr, onelags] = xcorr(word_in, one_ref);
-[two_corr, twolags] = xcorr(word_in, two_ref);
-[three_corr, threelags] = xcorr(word_in, three_ref);
-[four_corr, fourlags] = xcorr(word_in, four_ref);
-[five_corr, fivelags] = xcorr(word_in, five_ref);
-[six_corr, sixlags] = xcorr(word_in, six_ref);
-[seven_corr, sevenlags] = xcorr(word_in, seven_ref);
-[eight_corr, eightlags] = xcorr(word_in, eight_ref);
-[nine_corr, ninelags] = xcorr(word_in, nine_ref);
-[zero_corr, zerolags] = xcorr(word_in, zero_ref); 
+        [one_corr, onelags] = xcorr(word_in, one_ref);
+        [two_corr, twolags] = xcorr(word_in, two_ref);
+        [three_corr, threelags] = xcorr(word_in, three_ref);
+        [four_corr, fourlags] = xcorr(word_in, four_ref);
+        [five_corr, fivelags] = xcorr(word_in, five_ref);
+        [six_corr, sixlags] = xcorr(word_in, six_ref);
+        [seven_corr, sevenlags] = xcorr(word_in, seven_ref);
+        [eight_corr, eightlags] = xcorr(word_in, eight_ref);
+        [nine_corr, ninelags] = xcorr(word_in, nine_ref);
+        [zero_corr, zerolags] = xcorr(word_in, zero_ref); 
     end
-
-spike_loc = [find(zero_corr == max(zero_corr)) find(one_corr == max(one_corr))...
-    find(two_corr == max(two_corr)) find(three_corr == max(three_corr))...
-    find(four_corr == max(four_corr)) find(five_corr == max(five_corr))...
-    find(six_corr == max(six_corr)) find(seven_corr == max(seven_corr))...
-    find(eight_corr == max(eight_corr)) find(nine_corr == max(nine_corr))];
+    
+    spikes = [max(zero_corr) max(one_corr) max(two_corr) max(three_corr)...
+        max(four_corr) max(five_corr) max(six_corr) max(seven_corr)...
+        max(eight_corr) max(nine_corr)];
+    
+    spike_loc = [find(zero_corr == max(zero_corr)) find(one_corr == max(one_corr))...
+        find(two_corr == max(two_corr)) find(three_corr == max(three_corr))...
+        find(four_corr == max(four_corr)) find(five_corr == max(five_corr))...
+        find(six_corr == max(six_corr)) find(seven_corr == max(seven_corr))...
+        find(eight_corr == max(eight_corr)) find(nine_corr == max(nine_corr))];
 %match_loc is the locations of the **potentially** valid correlation spikes
-centerness = [abs(spike_loc(1) - len(1)), abs(spike_loc(2) - len(2)), abs(spike_loc(3) - len(3)),...
-            abs(spike_loc(4) - len(4)), abs(spike_loc(5) - len(5)), abs(spike_loc(6) - len(6)),...
-            abs(spike_loc(7) - len(7)), abs(spike_loc(8) - len(8)) abs(spike_loc(9) - len(9)),...
-            abs(spike_loc(10) - len(10))];
+    centerness = [abs(spike_loc(1) - len(1)), abs(spike_loc(2) - len(2)), abs(spike_loc(3) - len(3)),...
+                abs(spike_loc(4) - len(4)), abs(spike_loc(5) - len(5)), abs(spike_loc(6) - len(6)),...
+                abs(spike_loc(7) - len(7)), abs(spike_loc(8) - len(8)) abs(spike_loc(9) - len(9)),...
+                abs(spike_loc(10) - len(10))];
 %centerness is a vector containing a measure of how close to 'center' the
 %correlation for each reference is.  The closer to center their spike is,
 %the better the match.  Ideally, a perfect match will have a spike
 %perfectly in the center.
 
-match = find(centerness == min(centerness)) -1;
+    match = find(centerness == min(centerness)) -1;
 
-output(i) = match; %store the guess
-i = i+1;
+    output(i) = match; %store the guess
+    i = i+1;
 
-word_in = word_in(len(match+1):length(word_in));
+    word_in = word_in(len(match+1):length(word_in));
 
 
 end
