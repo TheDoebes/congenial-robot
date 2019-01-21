4b68739ee5b13b36601a94fa0a58c45e215711fa
masonparrish <45863229+masonparrish@users.noreply.github.com>
2019-01-21 14:32:03 -0500

    Add files via upload
    Program now works in detecting both single and multiword inputs when the inputs are identical to/composed of  the references. Re-recorded inputs do not consistently work.

--- Recognize_Speech_v3.m
+++ Recognize_Speech_v3.m
@@ -14,12 +14,26 @@ zero_ref = transpose(audioread('zero_ref.wav'));
 len = [length(zero_ref) length(one_ref) length(two_ref) length(three_ref) length(four_ref) length(five_ref)...
     length(six_ref) length(seven_ref) length(eight_ref) length(nine_ref)];
 %word_in = transpose(audioread('test_input.wav'));
-word_in  = [nine_ref one_ref one_ref];
+word_in  = [zero_ref one_ref three_ref four_ref five_ref six_ref seven_ref eight_ref nine_ref];
 word_in_original = word_in;
 %Begin the guessing
 
 i = 1;
 while (length(word_in)>min(len))
+    if(length(word_in) < min(len))
+    break
+    elseif(length(word_in) > max(len))
+[one_corr, onelags] = xcorr(word_in(1:len(2)), one_ref);
+[two_corr, twolags] = xcorr(word_in(1:len(3)), two_ref);
+[three_corr, threelags] = xcorr(word_in(1:len(4)), three_ref);
+[four_corr, fourlags] = xcorr(word_in(1:len(5)), four_ref);
+[five_corr, fivelags] = xcorr(word_in(1:len(6)), five_ref);
+[six_corr, sixlags] = xcorr(word_in(1:len(7)), six_ref);
+[seven_corr, sevenlags] = xcorr(word_in(1:len(8)), seven_ref);
+[eight_corr, eightlags] = xcorr(word_in(1:len(9)), eight_ref);
+[nine_corr, ninelags] = xcorr(word_in(1:len(10)), nine_ref);
+[zero_corr, zerolags] = xcorr(word_in(1:len(1)), zero_ref);
+    else %When the remaining word in is not longer than all references but not shorter than them all either
 [one_corr, onelags] = xcorr(word_in, one_ref);
 [two_corr, twolags] = xcorr(word_in, two_ref);
 [three_corr, threelags] = xcorr(word_in, three_ref);
@@ -29,7 +43,8 @@ while (length(word_in)>min(len))
 [seven_corr, sevenlags] = xcorr(word_in, seven_ref);
 [eight_corr, eightlags] = xcorr(word_in, eight_ref);
 [nine_corr, ninelags] = xcorr(word_in, nine_ref);
-[zero_corr, zerolags] = xcorr(word_in, zero_ref);
+[zero_corr, zerolags] = xcorr(word_in, zero_ref); 
+    end
 
 spike_loc = [find(zero_corr == max(zero_corr)) find(one_corr == max(one_corr))...
     find(two_corr == max(two_corr)) find(three_corr == max(three_corr))...
@@ -53,9 +68,7 @@ i = i+1;
 
 word_in = word_in(len(match+1):length(word_in));
 
-if(length(0:length(word_in)) < min(len))
-    break
-end
+
 end
 disp(output);
 
@@ -80,4 +93,3 @@ disp(output);
 % subplot(3,2,6)
 % plot(fourlags,four_corr)
 % title('Correlation of input against "Four"')
-plot(ninelags, nine_corr)
