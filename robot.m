7d8013e5935994d496fa4132bccfa78c6a412f88
TheDoebes <TheDoebes@users.noreply.github.com>
2019-01-21 15:45:21 -0500

    Create robot.m

---
+++ robot.m
@@ -0,0 +1,23 @@
+%Start
+
+%Get the filename to analyze from the user
+
+%Open the file
+
+%Opt: Remove Noise
+
+%Set t = trigger point
+
+%Loop while not end of file: 
+
+    %Cross-Correlate with audio sample library vector
+
+    %Print the highest correlation sample
+
+    %Set t = 0 to end of highest correlation score
+
+%End loop
+
+%Print the complete word or phrase
+
+%Exit
