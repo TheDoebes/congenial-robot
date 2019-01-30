7d9464853d5bcf93ce7ce41cf824f2dbaefe530f
masonparrish <45863229+masonparrish@users.noreply.github.com>
2019-01-29 22:04:48 -0500

    Add files via upload

---
+++ Master_Plot_v2.m
@@ -0,0 +1,283 @@
+% Making a master plot v2
+% figure
+% hold on
+% title('Master Plot: Every recording plotted by spectrogram')
+% figure
+% zero1 = audioread('zero.wav')';
+% spectrogram(zero1,70,'yaxis');
+% figure
+% zero2 = audioread('zero2.wav')';
+% spectrogram(zero2,70,'yaxis');
+% figure
+% zero3 = audioread('zero3.wav')';
+% spectrogram(zero3,70,'yaxis');
+% figure
+% zero4 = audioread('zero4.wav')';
+% spectrogram(zero4,70,'yaxis');
+% figure
+% zero5 = audioread('zero5.wav')';
+% spectrogram(zero5,70,'yaxis');
+% figure
+% zero6 = audioread('zero6.wav')';
+% spectrogram(zero6,70,'yaxis');
+% figure
+% zero7 = audioread('zero7.wav')';
+% spectrogram(zero7,70,'yaxis');
+% figure
+% zero8 = audioread('zero8.wav')';
+% spectrogram(zero8,70,'yaxis');
+% figure
+% zero9 = audioread('zero9.wav')';
+% spectrogram(zero9,70,'yaxis');
+% 
+% figure
+% one1 = audioread('one.wav')';
+% spectrogram(one1,70,'yaxis');
+% figure
+% one2 = audioread('one2.wav')';
+% spectrogram(one2,70,'yaxis');
+% figure
+% one3 = audioread('one3.wav')';
+% spectrogram(one3,70,'yaxis');
+% figure
+% one4 = audioread('one4.wav')';
+% spectrogram(one4,70,'yaxis');
+% figure
+% one5 = audioread('one5.wav')';
+% spectrogram(one5,70,'yaxis');
+% figure
+% one6 = audioread('one6.wav')';
+% spectrogram(one6,70,'yaxis');
+% figure
+% one7 = audioread('one7.wav')';
+% spectrogram(one7,70,'yaxis');
+% figure
+% one8 = audioread('one8.wav')';
+% spectrogram(one8,70,'yaxis');
+% figure
+% one9 = audioread('one9.wav')';
+% spectrogram(one9,70,'yaxis');
+% 
+% figure
+% two1 = audioread('two.wav')';
+% spectrogram(two1,70,'yaxis');
+% figure
+% two2 = audioread('two2.wav')';
+% spectrogram(two2,70,'yaxis');
+% figure
+% two3 = audioread('two3.wav')';
+% spectrogram(two3,70,'yaxis');
+% figure
+% two4 = audioread('two4.wav')';
+% spectrogram(two4,70,'yaxis');
+% figure
+% two5 = audioread('two5.wav')';
+% spectrogram(two5,70,'yaxis');
+% figure
+% two6 = audioread('two6.wav')';
+% spectrogram(two6,70,'yaxis');
+% figure
+% two7 = audioread('two7.wav')';
+% spectrogram(two7,70,'yaxis');
+% figure
+% two8 = audioread('two8.wav')';
+% spectrogram(two8,70,'yaxis');
+% figure
+% two9 = audioread('two9.wav')';
+% spectrogram(two9,70,'yaxis');
+% 
+figure
+three1 = audioread('three.wav')';
+spectrogram(three1,70,'yaxis');
+figure
+three2 = audioread('three2.wav')';
+spectrogram(three2,70,'yaxis');
+figure
+three3 = audioread('three3.wav')';
+spectrogram(three3,70,'yaxis');
+figure
+three4 = audioread('three4.wav')';
+spectrogram(three4,70,'yaxis');
+figure
+three5 = audioread('three5.wav')';
+spectrogram(three5,70,'yaxis');
+figure
+three6 = audioread('three6.wav')';
+spectrogram(three6,70,'yaxis');
+figure
+three7 = audioread('three7.wav')';
+spectrogram(three7,70,'yaxis');
+figure
+three8 = audioread('three8.wav')';
+spectrogram(three8,70,'yaxis');
+figure
+three9 = audioread('three9.wav')';
+spectrogram(three9,70,'yaxis');
+% 
+% figure
+% four1 = audioread('four.wav')';
+% spectrogram(four1,70,'yaxis');
+% figure
+% four2 = audioread('four2.wav')';
+% spectrogram(four2,70,'yaxis');
+% figure
+% four3 = audioread('four3.wav')';
+% spectrogram(four3,70,'yaxis');
+% figure
+% four4 = audioread('four4.wav')';
+% spectrogram(four4,70,'yaxis');
+% figure
+% four5 = audioread('four5.wav')';
+% spectrogram(four5,70,'yaxis');
+% figure
+% four6 = audioread('four6.wav')';
+% spectrogram(four6,70,'yaxis');
+% figure
+% four7 = audioread('four7.wav')';
+% spectrogram(four7,70,'yaxis');
+% figure
+% four8 = audioread('four8.wav')';
+% spectrogram(four8,70,'yaxis');
+% figure
+% four9 = audioread('four9.wav')';
+% spectrogram(four9,70,'yaxis');
+% 
+% figure
+% five1 = audioread('five.wav')';
+% spectrogram(five1,70,'yaxis');
+% figure
+% five2 = audioread('five2.wav')';
+% spectrogram(five2,70,'yaxis');
+% figure
+% five3 = audioread('five3.wav')';
+% spectrogram(five3,70,'yaxis');
+% figure
+% five4 = audioread('five4.wav')';
+% spectrogram(five4,70,'yaxis');
+% figure
+% five5 = audioread('five5.wav')';
+% spectrogram(five5,70,'yaxis');
+% figure
+% five6 = audioread('five6.wav')';
+% spectrogram(five6,70,'yaxis');
+% figure
+% five7 = audioread('five7.wav')';
+% spectrogram(five7,70,'yaxis');
+% figure
+% five8 = audioread('five8.wav')';
+% spectrogram(five8,70,'yaxis');
+% figure
+% five9 = audioread('five9.wav')';
+% spectrogram(five9,70,'yaxis');
+% 
+% figure
+% six1 = audioread('six.wav')';
+% spectrogram(six1,70,'yaxis');
+% figure
+% six2 = audioread('six2.wav')';
+% spectrogram(six2,70,'yaxis');
+% figure
+% six3 = audioread('six3.wav')';
+% spectrogram(six3,70,'yaxis');
+% figure
+% six4 = audioread('six4.wav')';
+% spectrogram(six4,70,'yaxis');
+% figure
+% six5 = audioread('six5.wav')';
+% spectrogram(six5,70,'yaxis');
+% figure
+% six6 = audioread('six6.wav')';
+% spectrogram(six6,70,'yaxis');
+% figure
+% six7 = audioread('six7.wav')';
+% spectrogram(six7,70,'yaxis');
+% figure
+% six8 = audioread('six8.wav')';
+% spectrogram(six8,70,'yaxis');
+% figure
+% six9 = audioread('six9.wav')';
+% spectrogram(six9,70,'yaxis');
+% 
+% figure
+% seven1 = audioread('seven.wav')';
+% spectrogram(seven1,70,'yaxis');
+% figure
+% seven2 = audioread('seven2.wav')';
+% spectrogram(seven2,70,'yaxis');
+% figure
+% seven3 = audioread('seven3.wav')';
+% spectrogram(seven3,70,'yaxis');
+% figure
+% seven4 = audioread('seven4.wav')';
+% spectrogram(seven4,70,'yaxis');
+% figure
+% seven5 = audioread('seven5.wav')';
+% spectrogram(seven5,70,'yaxis');
+% figure
+% seven6 = audioread('seven6.wav')';
+% spectrogram(seven6,70,'yaxis');
+% figure
+% seven7 = audioread('seven7.wav')';
+% spectrogram(seven7,70,'yaxis');
+% figure
+% seven8 = audioread('seven8.wav')';
+% spectrogram(seven8,70,'yaxis');
+% figure
+% seven9 = audioread('seven9.wav')';
+% spectrogram(seven9,70,'yaxis');
+% 
+% figure
+% eight1 = audioread('eight.wav')';
+% spectrogram(eight1,70,'yaxis');
+% figure
+% eight2 = audioread('eight2.wav')';
+% spectrogram(eight2,70,'yaxis');
+% figure
+% eight3 = audioread('eight3.wav')';
+% spectrogram(eight3,70,'yaxis');
+% figure
+% eight4 = audioread('eight4.wav')';
+% spectrogram(eight4,70,'yaxis');
+% figure
+% eight5 = audioread('eight5.wav')';
+% spectrogram(eight5,70,'yaxis');
+% figure
+% eight6 = audioread('eight6.wav')';
+% spectrogram(eight6,70,'yaxis');
+% figure
+% eight7 = audioread('eight7.wav')';
+% spectrogram(eight7,70,'yaxis');
+% figure
+% eight8 = audioread('eight8.wav')';
+% spectrogram(eight8,70,'yaxis');
+% figure
+% eight9 = audioread('eight9.wav')';
+% spectrogram(eight9,70,'yaxis');
+% 
+% figure
+% nine1 = audioread('nine.wav')';
+% spectrogram(nine1,70,'yaxis');
+% figure
+% nine2 = audioread('nine2.wav')';
+% spectrogram(nine2,70,'yaxis');
+% figure
+% nine3 = audioread('nine3.wav')';
+% spectrogram(nine3,70,'yaxis');
+% figure
+% nine4 = audioread('nine4.wav')';
+% spectrogram(nine4,70,'yaxis');
+% figure
+% nine5 = audioread('nine5.wav')';
+% spectrogram(nine5,70,'yaxis');
+% figure
+% nine6 = audioread('nine6.wav')';
+% spectrogram(nine6,70,'yaxis');
+% figure
+% nine7 = audioread('nine7.wav')';
+% spectrogram(nine7,70,'yaxis');
+% figure
+% nine8 = audioread('nine8.wav')';
+% spectrogram(nine8,70,'yaxis');
+% figure
+% nine9 = audioread('nine9.wav')';
+% spectrogram(nine9,70,'yaxis');
